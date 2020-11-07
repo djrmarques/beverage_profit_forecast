931068ed42687bd9f60cc0d3a6b1bbe9d32b6f48
david <david@david-t420.lan>
2020-11-07 19:17:02 +0000

    Finished the dashboard

--- app.py
+++ app.py
@@ -6,35 +6,67 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+from plot_functions import plot_cities_forecast, plot_profit_per_city, plot_profit_per_container
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+# Read the data
+df = pd.read_csv("data/profit_forecast/raw_data.csv", index_col=0)
+df.index = pd.to_datetime(df.index)
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+# Read the forecasting data
+forecasts = pd.read_csv("data/profit_forecast/forecasts.csv", index_col=0)
+forecasts.index = pd.to_datetime(forecasts.index)
 
+# Create the webapp
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='Beverage Sales Dashboard'),
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
+    dcc.Graph(id='Forecasting plot', figure=plot_cities_forecast(forecasts)),
 
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
+    html.P(),
+
+    dcc.Graph(id='profit-per-city'),
+    dcc.Slider(
+        id='city-slider',
+        min=df.index.year.min(),
+        max=df.index.year.max(),
+        value=df.index.year.max(),
+        marks={str(year): str(year) for year in df.index.year},
+        step=None
+    ),
+
+    dcc.Graph(id='profit-per-container'),
+    dcc.Slider(
+        id='container-slider',
+        min=df.index.year.min(),
+        max=df.index.year.max(),
+        value=df.index.year.max(),
+        marks={str(year): str(year) for year in df.index.year},
+        step=None
+    ),
+
+
 ])
+
+
+@app.callback(
+    Output('profit-per-container', 'figure'),
+    [Input('container-slider', 'value')])
+def update_cities(selected_year):
+    return plot_profit_per_city(selected_year, df)
+
+@app.callback(
+    Output('profit-per-city', 'figure'),
+    [Input('city-slider', 'value')])
+def update_containers(selected_year):
+    return plot_profit_per_container(selected_year, df)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
---
+++ plot_functions.py
@@ -0,0 +1,85 @@
+import numpy as np
+import pandas as pd 
+import plotly.express as px
+import plotly.graph_objects as go
+from typing import Dict
+import seaborn as sns
+
+def plot_cities_forecast(forecasts: Dict[str, pd.DataFrame]):
+    """ Creates the plotly figure for the cities forecast """
+    
+    # Get color scheme from seaborn
+    city_color_dictionary = dict(zip(forecasts["city"].unique(), [f"rgb{c}" for c in sns.color_palette("deep")]))
+
+    fig = go.Figure()
+    for city in forecasts["city"].unique():
+
+        # Plot real values
+        fig.add_trace(go.Scatter(x=forecasts[forecasts["city"]==city].index, 
+                                 y=forecasts.loc[forecasts["city"]==city, "real_values"], 
+                                 name=city, 
+                                 legendgroup=city,
+                                 line=dict(color=city_color_dictionary[city]),
+                                 mode='lines'))
+        # Plot Predictions
+        forecasting_df = forecasts.loc[(forecasts["city"]==city) & (forecasts["real_values"].isna()), :]
+        fig.add_trace(go.Scatter(x=forecasting_df.index, 
+                             y=forecasting_df["forecast"], 
+                             name=city, 
+                             legendgroup=city,
+                             mode='lines+markers',
+                             line=dict(color=city_color_dictionary[city], width=1, dash='dot'),
+                             error_y=dict(type='data', array=forecasting_df["error"], visible=True),    
+                             showlegend=False))
+        
+    # Add a vertical line to mark the forecasting period
+    fig.add_shape(type='line',
+                yref="paper",
+                xref="x",
+                x0=forecasts.loc[forecasts["city"]==city, "real_values"].dropna().index.max(),
+                y0=0,
+                x1=forecasts.loc[forecasts["city"]==city, "real_values"].dropna().index.max(),
+                y1=1,
+                line=dict(color='black', width=0.5, dash="dash"))
+    
+    # Add annoation on the forecast line 
+    fig.add_annotation(
+                x=forecasts.loc[forecasts["city"]==city, "real_values"].dropna().index.max(),
+                y=1,
+                yref='paper',
+                xanchor="right",
+                yanchor="top",
+                showarrow=False,
+                textangle=-90,
+                text="Forecast Start")
+
+    fig.layout.title.text = "Monthly Profit per city."
+    fig.layout.yaxis.title = "Profit (€)"
+    fig.layout.xaxis.title = "Date"
+    fig.update_xaxes(rangeslider_visible=True)
+    fig.update_layout(legend_title_text='City')
+    return fig
+
+def plot_profit_per_container(year: int, df: pd.DataFrame):
+    """ Plots the profit per container per brand on a given year"""
+    
+    df_ = df.drop(["lat", "long", "capacity"], axis=1).dropna().copy()
+    color_dict = dict(zip(["glass-500ml", 'plastic-1.5lt', 'can-330ml'], [f"rgb{c}" for c in sns.color_palette("deep")]))
+    
+    fig = px.box(df_[df_.index.year == year].dropna().sort_values("container"), x="brand", color="container", color_discrete_map=color_dict, y="profit", title=f"Monthly profit per brand per container on {year}")
+    fig.layout.yaxis.title = "Monthly Profit (€)"
+    fig.layout.xaxis.title = "Brand"
+    return fig
+
+def plot_profit_per_city(year: int, df: pd.DataFrame):
+    """ Plots the profit per city on a given year """
+    fig = px.bar(df.loc[df.index.year==year,:].groupby("city", as_index=False)["profit"].sum().sort_values("profit"), 
+                 x="city", 
+                 y="profit", 
+                 title=f"Total Profit per city on {year}",
+                 color_discrete_sequence = [f"rgb{sns.color_palette('deep')[0]}"]
+                )
+    
+    fig.layout.yaxis.title="Profit (€)"
+    fig.layout.xaxis.title="City"
+    return fig
